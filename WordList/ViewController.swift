0965ee2139cb5267705a45ca9a287d92ef9358a1
Vlad <sorvigolovaqwerty@gmail.com>
2019-04-03 21:31:11 +0300

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Vlad on 03.04.2019.
+//  Copyright © 2019 Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
